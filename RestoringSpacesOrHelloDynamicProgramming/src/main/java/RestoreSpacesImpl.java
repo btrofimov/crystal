1f23b77b9f0c73e835bb3f621049b47efc05e2cc
Boris Trofimov <boris.trofimov@sigmaukraine.com>
2015-03-11 13:51:53 +0200

    Update RestoreSpacesImpl.java

--- RestoringSpacesOrHelloDynamicProgramming/src/main/java/RestoreSpacesImpl.java
+++ RestoringSpacesOrHelloDynamicProgramming/src/main/java/RestoreSpacesImpl.java
@@ -14,21 +14,22 @@ class RestoreSpacesImpl{
 
 	/**
 	 *  Major API method to restore spaces.
-	 * @param input strign without spaces
+	 * @param input string without spaces
 	 * @return recovered string
 	 */
 	public String restoreSpaces(String input){
+		// create indexes
 		List<Integer> previous = buildMap(input);
-		
+		// restore string by coresponding list of indexes
 		return restoreString(input, previous);
 	} 
 	
 	/**
 	 * Method builds list of space indexes for particular string
-	 * Implementation uses classic dynamic programming approach based on  O(n^2)
+	 * Implementation uses classic dynamic programming approach based on  O(n^2), max function is number of spaces
 	 * However there are still many places where possible to tune performance, like arr.addAll(...)
 	 * @param input
-	 * @return
+	 * @return list of indexes where spaces have to be placed
 	 */
 	protected List<Integer> buildMap(String input){
 		if(input.length()==0)
@@ -37,12 +38,14 @@ class RestoreSpacesImpl{
 		List<List<Integer>> previous = new ArrayList<>(input.length());		
 		// the first major cycle, on every step i we find solution for substring length i
 		for(int i = 0;i<input.length();i++){
+			// max criteria is number of spaces for current solution
 			int max = 0;
 			Integer max_j = null; 
 			// the second cycle, trying to find such j where this combination is the best: 
 			// [<previous best solution>,<index j>,<word in dictionary>,<index i>,...<rest of input string>]
 			for(int j = 0; j<=i; j++){
-				if(dict.contains(input.substring(j,i+1)) && (j==0 || previous.get(j-1).size()>0)){
+				// skip zero solutions or if word [j,..,i] does not contain in dict
+				if(dict.contains(input.substring(j,i+1)) && (j==0 || previous.get(j-1).size()>0)){ // skip all zero solutions
 					
 					int prev_max = j==0?0:previous.get(j-1).size();
 					
@@ -53,6 +56,7 @@ class RestoreSpacesImpl{
 				}
 			}
 			// build list indexes for current i-step solution
+			// empty list means penalty for i-step with coresponding max value 0 , if we have not succeeded
 			List<Integer> arr = new LinkedList<>();
 			if(max_j != null){
 				if(max_j>0)
@@ -62,7 +66,7 @@ class RestoreSpacesImpl{
 			}
 			previous.add(arr);
 		}	
-		// return the solution for last step for char with index (input.length-1) which is basicly the best solution for entire input stringg
+		// return the solution (in this case its list of indexes) for last step for char with index (input.length-1) which is basicly the best solution for entire input stringg
 		return previous.get(previous.size()-1);
 	}
 	
